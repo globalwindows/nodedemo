1e1477c70758c712f63716d40b6d6283a3960862
CharlesBaldo <charlesbaldo@gmail.com>
2015-02-16 12:29:26 -0500

    added hello world demo file

---
+++ HelloWorld.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var html;
+html ="<h1>Hello World</h1>";
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(html);
+  res.end();
+}).listen(3000, "127.0.0.1");
+console.log('Server running at http://localhost:3000/');
